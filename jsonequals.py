b2d0fa60d798e2dc8eaff89b4e878abc89697293
Jason Prendergast <j1mbob200@gmail.com>
2018-04-22 19:55:12 +0100

    Refactor: pep8 spaces
    Refactor: pep8 spaces

--- jsonequals.py
+++ jsonequals.py
@@ -1,12 +1,12 @@
 class jsonequals():
-    def __init__(self,testjson):
+    def __init__(self, testjson):
         self.testjson=testjson
 
     def jsonequals(self):
         a = self.testjson['value1']
         b = self.testjson['value2']
         if a and b:
-            if a!=b:
+            if a != b:
                 return False
             for i in range(len(a)):
                 if a[i] != b[i]:
